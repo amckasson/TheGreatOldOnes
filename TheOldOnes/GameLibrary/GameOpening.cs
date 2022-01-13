458e8bd6f4e43208cb3b19cb8af5556bbb0e2578
amckasson90@gmail.com <amckasson90@gmail.com>
2022-01-12 23:31:35 -0500

    Init commit

---
+++ TheOldOnes/GameLibrary/GameOpening.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheOldOnes.GameLibrary
+{
+    public class GameOpening
+    {
+        public void GameTitle()
+        {
+            Console.WriteLine(@"
+    
+    ░██████╗░██████╗░███████╗░█████╗░████████╗  ░█████╗░██╗░░░░░██████╗░  ░█████╗░███╗░░██╗███████╗░██████╗
+    ██╔════╝░██╔══██╗██╔════╝██╔══██╗╚══██╔══╝  ██╔══██╗██║░░░░░██╔══██╗  ██╔══██╗████╗░██║██╔════╝██╔════╝
+    ██║░░██╗░██████╔╝█████╗░░███████║░░░██║░░░  ██║░░██║██║░░░░░██║░░██║  ██║░░██║██╔██╗██║█████╗░░╚█████╗░
+    ██║░░╚██╗██╔══██╗██╔══╝░░██╔══██║░░░██║░░░  ██║░░██║██║░░░░░██║░░██║  ██║░░██║██║╚████║██╔══╝░░░╚═══██╗
+    ╚██████╔╝██║░░██║███████╗██║░░██║░░░██║░░░  ╚█████╔╝███████╗██████╔╝  ╚█████╔╝██║░╚███║███████╗██████╔╝
+    ░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝░░░╚═╝░░░  ░╚════╝░╚══════╝╚═════╝░  ░╚════╝░╚═╝░░╚══╝╚══════╝╚═════╝░
+
+");
+            Console.WriteLine("\n\n\n\t\t\t\t\t\t1. Start Game\n\n" +
+                "\t\t\t\t\t\t2. Laws of Tera\n\n" +
+                "\t\t\t\t\t\t3. Quit");
+
+
+            string input = Console.ReadLine();
+
+            switch (input.ToLower())
+            {
+                case "1":
+                    // StartGame
+                    break;
+                case "2":
+                    // DisplayRules
+                    break;
+                case "3":
+                    // Exit
+                    //                    keepRunning = false;
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("\n\tPlease enter a valid option.\n" +
+                        "\n" +
+                        "\tPress any key to continue...");
+                    Console.ReadKey();
+                    break;
+            }
+
+
+        }
+
+
+    }
+}
---
+++ TheOldOnes/ProgramUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheOldOnes.GameLibrary;
+
+namespace TheOldOnes
+{
+    public class ProgramUI
+    {
+        public void Run()
+        {
+            Menu();
+        }
+
+        private void Menu()
+        {
+            GameOpening opening = new GameOpening();
+            opening.GameTitle();
+
+        }
+    }
+}
